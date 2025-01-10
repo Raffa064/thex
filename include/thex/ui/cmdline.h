48c2a753dea748e950a8fe24d4a1dbbdf8c5d562
Raffa064 <raffaddtg@gmail.com>
2025-01-10 20:35:41 -0300

    Fixed wrong gitignore rule

---
+++ include/thex/ui/cmdline.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "input.h"
+#include "ui.h"
+#include <functional>
+#include <string>
+#include <vector>
+
+#define CommandFunction std::function<void (std::string, std::string&)>
+
+struct Command {
+  std::string name;
+  CommandFunction fn;
+};
+
+class THexCommandLine : public UI, public InputReceiver {
+  char* buffer = nullptr;
+  std::string command_output;
+  std::vector<Command> commands;
+
+  void start_input();
+
+public:
+
+  void add_cmd(std::string, CommandFunction);
+  
+  void draw() override;
+  bool accept(Event) override;
+
+  THexCommandLine();
+
+  ~THexCommandLine();
+
+  THexCommandLine& operator=(THexCommandLine other) {
+    buffer = other.buffer;
+    commands = other.commands;
+    command_output = other.command_output;
+
+    return *this;
+  }
+};
